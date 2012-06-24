d5ee1377bf72977ef472b9d1ccaff2f58206b161
Philip Brechler <pbrechler@mac.com>
2012-06-24 17:23:39 +0200

    Intention problems

--- livestream/views.py
+++ livestream/views.py
@@ -13,12 +13,12 @@ def current(request):
     ''' This view gets all streams that are scheduled to be live right now
     if there are no streams it redirects to the liste view. The upcoming_streams_list
     is used to show upcoming events in a side bar (see the template)'''
-	stream_list = Stream.objects.filter(published=True,startDate__lt=datetime.datetime.now, endDate__gt=datetime.datetime.now).order_by('-startDate')
-	upcoming_streams_list = Stream.objects.filter(published=True,endDate__gt=datetime.datetime.now).order_by('-startDate')[:5]
-	if not stream_list:
-		return redirect(list)
-	else:
-		return render_to_response('livestream/current.html', {'stream_list': stream_list, 'upcoming_streams_list': upcoming_streams_list},
+    stream_list = Stream.objects.filter(published=True,startDate__lt=datetime.datetime.now, endDate__gt=datetime.datetime.now).order_by('-startDate')
+    upcoming_streams_list = Stream.objects.filter(published=True,endDate__gt=datetime.datetime.now).order_by('-startDate')[:5]
+    if not stream_list:
+        return redirect(list)
+    else:
+        return render_to_response('livestream/current.html', {'stream_list': stream_list, 'upcoming_streams_list': upcoming_streams_list},
                             context_instance=RequestContext(request))
 
 
