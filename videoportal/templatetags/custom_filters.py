ea400ffa3300140bc2d4b0f9cbb2011b2f5580ef
Philip Brechler <pbrechler@mac.com>
2012-06-24 17:22:14 +0200

    Missed one

--- videoportal/templatetags/custom_filters.py
+++ videoportal/templatetags/custom_filters.py
@@ -5,6 +5,6 @@ register = template.Library()
 
 @register.filter(name='secondstohms')
 def secondstohms(value):
-''' This is used to have a nicer format for the video duration in the template'''
+    ''' This is used to have a nicer format for the video duration in the template'''
     if (value):
         return str(datetime.timedelta(seconds=int(value)))
